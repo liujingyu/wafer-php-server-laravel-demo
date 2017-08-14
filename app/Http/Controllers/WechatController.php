2bb9e5f62e82bc41d75fcb37d8ba11ff23c4a896
liujingyu <jingyu525@gmail.com>
2017-08-14 21:00:43 +0800

    perf: WechatController

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -6,12 +6,21 @@ use Illuminate\Http\Request;
 use QCloud_WeApp_SDK\Auth\LoginService as LoginService;
 use QCloud_WeApp_SDK\Tunnel\TunnelService as TunnelService;
 use App\Business\ChatTunnelHandler;
+use Log;
 
 class WechatController extends Controller
 {
-	public function login()
-	{
-		$result = LoginService::login();
+    public function login()
+    {
+        $result = LoginService::login();
+
+        if ($result['code'] === 0) {
+            // 微信用户信息：`$result['data']['userInfo']`
+            Log::info('[登录成功微信]', $result);
+        } else {
+            Log::info('[登录失败微信]', $result);
+            // 登录失败原因：`$result['message']`
+        }
 	}
 
 	public function user()
@@ -28,7 +37,8 @@ class WechatController extends Controller
 				'userInfo' => $result['data']['userInfo'],
 			),
 		);
-		echo json_encode($response, JSON_FORCE_OBJECT);
+        return response()->json($response);
+		// echo json_encode($response, JSON_FORCE_OBJECT);
     }
 
     public function tunnel()
